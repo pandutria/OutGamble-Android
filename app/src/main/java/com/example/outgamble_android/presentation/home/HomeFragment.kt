0008afd928ebd5e3385fbdb13ef85128f7d37987
pandutria <panduadyatama029@gmail.com>
2025-12-24 20:51:54 +0700

    Initial commit

--- app/src/main/java/com/example/outgamble_android/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/outgamble_android/presentation/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.outgamble_android.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import com.example.outgamble_android.data.model.News
 import com.example.outgamble_android.data.state.ResultState
 import com.example.outgamble_android.databinding.FragmentHomeBinding
 import com.example.outgamble_android.presentation.adapter.NewsAdapter
+import com.example.outgamble_android.presentation.news.NewsActivity
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -32,6 +34,12 @@ class HomeFragment : Fragment() {
         binding.tvFullname.text = "Hi, ${FullnamePref(requireContext()).get()}"
 
         requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.primary)
+
+        newsAdapter = NewsAdapter { news ->
+            val intent = Intent(requireActivity(), NewsActivity::class.java)
+            intent.putExtra("link", news.link)
+            startActivity(intent)
+        }
 
         viewModel.getNews()
         viewModel.newsState.observe(viewLifecycleOwner) {state ->
---
+++ app/src/main/java/com/example/outgamble_android/presentation/news/NewsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.outgamble_android.presentation.news
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.outgamble_android.R
+import com.example.outgamble_android.databinding.ActivityMainBinding
+
+class NewsActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
